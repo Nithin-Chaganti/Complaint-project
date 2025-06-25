71e68864e6636327328a1930686814a3efea7689
Nithin-Chaganti <nithinchaganti209@gmail.com>
2025-06-25 13:19:35 +0530

    Add files via upload

---
+++ Complaint-Project/frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/user', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// ...existing code...
